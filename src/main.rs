16613f422d8b8ee344c52fab4658ee86a38057b6
Anto Keinanen <anto.keinanen@gmail.com>
2022-09-06 14:53:42 +0300

    Reworked object loading for better performance and larger supported models.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::f32::consts::PI;
 
 use copper_engine::{
     blank_on_awake, blank_tick_update, engine, model_loading, structs::Scene,
-    AudioSourceLocal, Camera, InputManager, Object,
+    Camera, InputManager, Object,
 };
 use model_loading::model_loader::get_obj;
 
@@ -20,7 +20,7 @@ fn main() {
 
     let dragon = Object::new(
         "Dragon",
-        get_obj("models/high_res_speher.obj"),
+        get_obj("models/stanford_dragon_low.obj"),
         [0.0, 0.0, 0.0],
         [0.0, 0.0, 0.0],
         [1.0, 1.0, 1.0],
@@ -28,11 +28,9 @@ fn main() {
         blank_on_awake,
     );
 
-    let audio_source = AudioSourceLocal::new("audio/beep2.wav", 1.0, false, [10.0, 0.0, 0.0], 50.0);
-
     let scene: Scene = Scene::new(
         vec![dragon],
-        vec![audio_source],
+        vec![],
         vec![],
         input_manager,
         main_camera,
@@ -42,8 +40,8 @@ fn main() {
 }
 
 fn dragon_tick_update(scene: &mut Scene) {
-    scene.game_objects[0].rotation[0] = scene.game_objects[0].rotation[0] + 10.0 * scene.delta_time;
-    scene.game_objects[0].rotation[1] = scene.game_objects[0].rotation[1] + 10.0 * scene.delta_time;
+    scene.game_objects[0].rotation[0] = scene.game_objects[0].rotation[0] + 100.0 * scene.delta_time;
+    scene.game_objects[0].rotation[1] = scene.game_objects[0].rotation[1] + 100.0 * scene.delta_time;
 
     if scene.game_objects[0].rotation[0] > 2.0 * PI {
         scene.game_objects[0].rotation[0] = 0.0;
--- src/model_loading/model_loader.rs
+++ src/model_loading/model_loader.rs
@@ -1,9 +1,6 @@
-use obj::{load_obj, Obj};
-use std::fs::File;
-use std::io::BufReader;
-
 use crate::structs::vertex::{Normal, Vertex};
 
+// **Should not be created by user: use `::get_obj()`**
 #[derive(Debug)]
 pub struct Model {
     pub positions: Vec<Vertex>,
@@ -12,37 +9,92 @@ pub struct Model {
 }
 
 /// loads and parses wavefront OBJ file into copper engine compatible format.
-/// 
-/// # Errors
-/// - Path provided does not already exist.
-/// - IO error has been occurred during opening the `obj` file.
-/// - Tried to parse integer from the `obj` file, but failed.
-/// - Tried to parse floating point number from the `obj` file, but failed.
-/// - `LoadError` has been occurred during parsing the `obj` file.
-/// - Other errors may also be returned according to [`OpenOptions::open`].
-/// 
+///
 /// # Examples
 /// ```
 /// let model = get_obj("path/to/the/model.obj");
 /// ```
-pub fn get_obj(path:&str) -> Model {
-    let input = BufReader::new(File::open(path).expect("Unable to open path!"));
-    let loaded: Obj = load_obj(input).expect("Failed to load obj from provided buffer reader!");
+pub fn get_obj(path: &str) -> Model {
+    let (models, materials) = tobj::load_obj(
+        path,
+        &tobj::LoadOptions {
+            single_index: true,
+            ..Default::default()
+        },
+    )
+    .expect(format!("Failed to load or parse provided obj: {}", path).as_str());
 
-    let mut model = Model {
-        positions: vec![],
-        normals: vec![],
-        indices: loaded.indices,
-    };
+    let model = &models[0];
 
-    for vertex in loaded.vertices {
-        model.positions.push(Vertex {
-            position: vertex.position,
-        });
-        model.normals.push(Normal {
-            normal: vertex.normal,
+    let m_vtx = &model.mesh.positions;
+    let m_nor = &model.mesh.normals;
+    let m_ind = &model.mesh.indices;
+
+    let mut vertexes: Vec<Vertex> = vec![];
+    let mut normals: Vec<Normal> = vec![];
+    let mut indices: Vec<u16> = vec![];
+
+    assert!(m_vtx.len() % 3 == 0);
+    assert!(m_nor.len() % 3 == 0);
+
+    for vtx in 0..m_vtx.len() / 3 {
+        vertexes.push(Vertex {
+            position: [m_vtx[3 * vtx], m_vtx[3 * vtx + 1], m_vtx[3 * vtx + 2]],
         });
     }
-    
-    model
+
+    for nor in 0..model.mesh.normals.len() / 3 {
+        normals.push(Normal {
+            normal: [m_nor[3 * nor], m_nor[3 * nor + 1], m_nor[3 * nor + 2]],
+        })
+    }
+
+    for ind in m_ind {
+        indices.push(*ind as u16);
+    }
+
+    Model {
+        positions: vertexes,
+        normals,
+        indices,
+    }
 }
+
+// let (models, materials) =
+// tobj::load_obj(&path, &tobj::LoadOptions::default()).expect("Failed to load OBJ file.");
+
+// //let materials = Material::default();
+
+// let mp = &models[0].mesh.positions;
+// let mn = &models[0].mesh.normals;
+// let mi = &models[0].mesh.indices;
+
+// let mut positions: Vec<Vertex> = vec![];
+
+// assert!(mp.len() % 3 == 0);
+
+// for vtx in 0..mp.len() / 3 {
+// positions.push(Vertex {
+//     position: [mp[3 * vtx], mp[3 * vtx + 1], mp[3 * vtx + 2]],
+// });
+// }
+
+// assert!(mn.len() % 3 == 0);
+
+// let mut normals: Vec<Normal> = vec![];
+// for nor in 0..mn.len() / 3 {
+// normals.push(Normal {
+//     normal: [mn[3 * nor], mn[3 * nor + 1], mn[3 * nor + 2]],
+// });
+// }
+
+// let mut indices: Vec<u16> = vec![];
+// for ind in mi {
+// indices.push(*ind as u16);
+// }
+
+// Model {
+// positions,
+// normals,
+// indices,
+// }
